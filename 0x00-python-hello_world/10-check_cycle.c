9479db6878c58c29392a6b72ef31b776c021e499
MachinEmmus <emonsalvep38@gmail.com>
2019-12-10 00:08:41 -0500

    Add task mandatory 10 almost

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *head)
 	listint_t *hare = head;
 	listint_t *turtle = head;
 
-	while (head && turtle->next && hare->next->next)
+	while (head && hare->next && hare->next->next)
 	{
 		hare = hare->next->next;
 		turtle = turtle->next;
