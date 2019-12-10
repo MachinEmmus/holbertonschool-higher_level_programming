c5e56c711b5e5ae29118e09673e433d3dfbf3b11
MachinEmmus <emonsalvep38@gmail.com>
2019-12-09 19:07:31 -0500

    efficiense

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,11 +9,7 @@ int check_cycle(listint_t *head)
 {
 	listint_t *hare = head;
 	listint_t *turtle = head;
-	
-	if (!head)
-		return (0);
-
-	while(turtle->next && hare->next->next)
+	while(head && turtle->next && hare->next->next)
 	{
 		turtle = turtle->next;
 		hare = hare->next->next;
