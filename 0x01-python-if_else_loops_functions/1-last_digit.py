49e4daed4c2993c8badebcfb1d6614d06c009071
MachinEmmus <emonsalvep38@gmail.com>
2019-12-10 18:35:52 -0500

    |In this case i solved the peptty style and learn about this|

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+s = "Last digit of "
 if number < 0:
-	num = number * -1
+    num = number * -1
 else:
-	num = number
+    num = numberi
 if ((num % 10) > 5):
-	print('Last digit of {} is {} and is greater than 5'.format(number, num % 10))
+    print(s + '{} is {} and is greater than 5'.format(number, num % 10))
 elif ((num % 10) == 0):
-	print('Last digit of {} is {} and is 0'.format(number, num % 10))
+    print(s + '{} is {} and is 0'.format(number, num % 10))
 else:
-	print('Last digit of {} is {} and is less than 6 and not 0'.format(number, num % 10))
+    print(s + '{} is {} and is less than 6 and not 0'.format(number, num % 10))
