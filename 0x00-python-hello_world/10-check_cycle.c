f0265d6dac790533f11f7358fe874bc36a2fdf22
MachinEmmus <emonsalvep38@gmail.com>
2019-12-09 23:58:14 -0500

    add |fixed the task|

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * check_cycle - check if a loop is infinity
+ * @head: is the head of my linked list
+ * Return: 0 is not loop, 1 is a loop
  */
 
 int check_cycle(listint_t *head)
 {
 	listint_t *hare = head;
 	listint_t *turtle = head;
-	while(head && turtle->next && hare->next->next)
+
+	while (head && turtle->next && hare->next->next)
 	{
-		turtle = turtle->next;
 		hare = hare->next->next;
+		turtle = hare->next;
 		if (turtle == hare)
 			return (1);
 	}
