5ec9a4bad172d29aaa921f0cdf98f83e03a66c60
MachinEmmus <emonsalvep38@gmail.com>
2019-12-10 18:38:19 -0500

    I have a missclick i am stupid

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ s = "Last digit of "
 if number < 0:
     num = number * -1
 else:
-    num = numberi
+    num = number
 if ((num % 10) > 5):
-    print(s + '{} is {} and is greater than 5'.format(number, num % 10))
+    print(s + "{} is {} and is greater than 5".format(number, num % 10))
 elif ((num % 10) == 0):
-    print(s + '{} is {} and is 0'.format(number, num % 10))
+    print(s + "{} is {} and is 0".format(number, num % 10))
 else:
-    print(s + '{} is {} and is less than 6 and not 0'.format(number, num % 10))
+    print(s + "{} is {} and is less than 6 and not 0".format(number, num % 10))
