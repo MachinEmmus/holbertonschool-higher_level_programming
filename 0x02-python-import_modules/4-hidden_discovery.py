3506e1c7f6a01acba2bdca90467db4d848ee8b27
MachinEmmus <emonsalvep38@gmail.com>
2019-12-12 15:58:02 -0500

    add task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+    for i in range(0, len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != "_":
+            print(dir(hidden_4)[i])
