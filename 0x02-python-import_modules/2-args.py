cd67e19f0ed15c824b9d719bb7167635b51671a6
MachinEmmus <emonsalvep38@gmail.com>
2019-12-12 15:09:04 -0500

    |fixed pepstyle in task 2|, add task 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":    
+if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
         if len(sys.argv) == 2:
             print("1 argument:")
-            print("{:d}: {}".format(len(sys.argv) -1, sys.argv[1]))
+            print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
         else:
             print("{:d} arguments:".format(len(sys.argv) - 1))
             for i in range(1, len(sys.argv)):
