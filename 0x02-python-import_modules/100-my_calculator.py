254952c3e71cb62ee4f086efb8ffc20f58cc0b9d
MachinEmmus <emonsalvep38@gmail.com>
2019-12-12 19:20:29 -0500

    |fixed tge dummies errors wit copy and paste|

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
         if sys.argv[2] == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
-            print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
