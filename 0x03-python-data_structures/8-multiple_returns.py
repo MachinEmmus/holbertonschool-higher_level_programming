ad6f2a5df5ceccf2087e84e6cbea4e7feb1b04d0
MachinEmmus <emonsalvep38@gmail.com>
2019-12-17 20:41:09 -0500

    fixed bugs

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiple_return(sentence):
-    size = len(sentece)
+def multiple_returns(sentence):
+    size = len(sentence)
     if size == 0:
         return (0, None)
     return (size, sentence[0])
