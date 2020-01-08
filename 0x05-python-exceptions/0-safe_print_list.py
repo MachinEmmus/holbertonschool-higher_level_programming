6934a623dfb757380a9f04eb01c98b36b9ce96ab
MachinEmmus <emonsalvep38@gmail.com>
2020-01-08 13:24:12 -0500

    add task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(0, x):
@@ -8,5 +9,3 @@ def safe_print_list(my_list=[], x=0):
             continue
     print()
     return count
-            
-    
