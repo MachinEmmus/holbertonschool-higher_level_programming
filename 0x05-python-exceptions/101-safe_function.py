737f2602fc4c3d8dbd91b214261f6a0dc8318b9a
MachinEmmus <emonsalvep38@gmail.com>
2020-01-08 23:47:41 -0500

    Advanced Task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        return (fct(*args))
+    except Exception as e:
+        stderr.write("Exception: " + str(e) + "\n")
+        return None
+        
