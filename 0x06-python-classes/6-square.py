ee6a98b4eb8baf5b315c25927bbad1fc38b9bcf5
MachinEmmus <emonsalvep38@gmail.com>
2020-01-09 23:43:45 -0500

    add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+class Square:
+    """
+        This is my class Square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        The init function in my class
+        Args:
+            size (int): Is the size of my square
+        """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and len(value) == 2:
+            if value[0] >= 0 and value[1] >= 0:
+                self.__position = value
+                return
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.position[1]):
+            print()
+        for j in range(self.__size):
+            print("{}{} ".format(" " * self.position[0], "#" * self.__size))
