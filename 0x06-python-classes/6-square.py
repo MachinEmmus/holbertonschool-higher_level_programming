75da19cccc3d78c5bd80281e4a2423bcddf9aaa0
MachinEmmus <emonsalvep38@gmail.com>
2020-01-09 23:46:05 -0500

    add task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.position[1]):
+        for i in range(self.__position[1]):
             print()
         for j in range(self.__size):
-            print("{}{} ".format(" " * self.position[0], "#" * self.__size))
+            print("{}{} ".format(" " * self.__position[0], "#" * self.__size))
