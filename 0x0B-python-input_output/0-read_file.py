4f0988995c042779d4fbc962b3b157cd99139796
MachinEmmus <emonsalvep38@gmail.com>
2020-01-21 18:20:07 -0500

    parametrer error in with i put the same name

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open('my_file_0.txt') as f:
+    with open(filename) as f:
         for line in f:
             print(line, end="")
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     count = 0
-    with open('my_file_0.txt') as f:
+    with open(filename) as f:
         for line in f:
             print(line, end="")
             count += 1
