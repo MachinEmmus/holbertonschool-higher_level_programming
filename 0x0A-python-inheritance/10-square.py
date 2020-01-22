c72b2ebf0f1e946c23c53baceb960d749d2932fc
MachinEmmus <emonsalvep38@gmail.com>
2020-01-22 16:54:37 -0500

    The init is the last time

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,11 +4,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """"""
-
     def __init__(self, size):
         self.__size = size
-        super().__init__(size, size)
         super().integer_validator("size", size)
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
