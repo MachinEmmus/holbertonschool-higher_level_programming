f3b57ec540b868c1342e4a3ee1e5131b024e7280
MachinEmmus <emonsalvep38@gmail.com>
2020-01-30 20:49:15 -0500

    Add task 16 with emmus

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,8 +19,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """dont forget comments"""
+        """serialization a json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+ 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_file in the cls"""
+        with open(cls.__name__ + ".json", "w") as f:
+            my_list = []
+            if list_objs is None:
+                return f.write(cls.to_json_string(None))
+            else:
+                for i in list_objs:
+                    my_list.append(i.to_dictionary())
+                list_in_json = cls.to_json_string(my_list)
+            return f.write(list_in_json)
