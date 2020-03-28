c90d4d93fccba6334fd43b012cf78bc76eaf965e
Emmus Monsalve <emonsalvep38@gmail.com>
2020-03-28 17:49:19 -0500

    Try to solve errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-""" Scripts that list all register that start with 'n'""" 
+""" Scripts that list all register that start with 'n'"""
 
 if __name__ == "__main__":
 
     import sys
-    import MySQLdb
+    if (len(sys.argv) == 4):
+        import MySQLdb
 
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        db_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        user=user,
-        passwd=passwd,
-        db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE \"n%\" ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+        db = MySQLdb.connect(
+            host="localhost",
+            user=user,
+            passwd=passwd,
+            db=db_name)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name\
+                     LIKE 'n%' ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
