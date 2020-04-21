de486da9e94ace47a6d925e0b81dddfab6254a6a
Emmus Monsalve <emonsalvep38@gmail.com>
2020-04-21 15:11:30 -0500

    i found my error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     email = sys.argv[2]
     value = {'email': email}
     data = urllib.parse.urlencode(value)
-    data.encode('ascii')
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(req) as response:
         html = response.read()
     print(response.decode("urf-8"))
         
